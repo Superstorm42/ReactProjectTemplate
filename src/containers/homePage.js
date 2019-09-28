ff383fb6ec91f1f6714d71fff510c121d1a9f6c5
Superstorm42 <riasatali4242@gmail.com>
2019-09-28 14:18:01 +0600

    Add files via upload

---
+++ src/containers/homePage.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+class HomePage extends Component{
+    render(){
+        return (
+            <div>Edit this page as homepage and create other pages and routes.</div>
+        )
+    }
+}
+export default HomePage;
